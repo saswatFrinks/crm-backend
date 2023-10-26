9468653c86e1239edc444326746e28c448f41df8
parth-frinks <parth.ambre@frinks.ai>
2023-10-26 11:19:14 +0530

    tests added

---
+++ jest.setup.ts
@@ -0,0 +1,4 @@
+
+process.env.ENV = 'test'
+
+console.log('-----SETUP----')
--- unit.jest.config.js
+++ unit.jest.config.js
@@ -1,10 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   preset: 'ts-jest',
   roots: ['./test-unit'],
   verbose: true,
+  setupFiles: ['./jest.setup.ts'],
   testEnvironment: 'node',
-  fakeTimers: {
-    enableGlobally: true,
-  },
 }
