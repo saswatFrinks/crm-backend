8b6bfeb3e9671652261f55cddaff227f3bcd45b4
Parth Ambre <parth.ambre@frinks.ai>
2024-03-18 10:29:47 +0530

    ping removed

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import PingController from '../controller/ping'
 
 const routes = Router()
-
-routes.get('/ping', PingController.get)
 
 export default routes
